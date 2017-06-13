448d6d9f4243a0ba664467635a641568e7bc455f
tpctt <491590253@qq.com>
2017-06-13 15:34:12 +0800

    [TASK] 支持 2.3.1 的 carthage

---
+++ ADViewDemo/DesktopADView/DesktopADView.h
@@ -0,0 +1,19 @@
+//
+//  DesktopADView.h
+//  DesktopADView
+//
+//  Created by tim on 2017/6/13.
+//  Copyright © 2017年 ISNC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DesktopADView.
+FOUNDATION_EXPORT double DesktopADViewVersionNumber;
+
+//! Project version string for DesktopADView.
+FOUNDATION_EXPORT const unsigned char DesktopADViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DesktopADView/PublicHeader.h>
+
+
